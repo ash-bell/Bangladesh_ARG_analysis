d66a43032ed41ff0d6505589fe60385dae8dd3d3
Ashley <44369148+ash-bell@users.noreply.github.com>
2022-06-24 17:52:33 +0100

    Update and rename 07_ARG_read_plasmid_preprocessing.R to 07_ARG_read_plasmid__KEGG_preprocessing.R

--- 07_ARG_read_plasmid__KEGG_preprocessing.R
+++ 07_ARG_read_plasmid__KEGG_preprocessing.R
@@ -97,3 +97,58 @@ plas_strict = plasmids %>%
 
 # remove the word "antibiotic" and any spaces from the Drug.Class column
 plas_strict$Drug.Class = str_trim(gsub("antibiotic", "", plas_strict$Drug.Class), side = c("both"))
+
+###
+### Preprocessing KEGGs
+
+### see https://merenlab.org/2018/01/17/importing-ghostkoala-annotations/#generate-the-kegg-orthology-table for details 
+### this section in bash ###
+
+wget 'https://www.genome.jp/kegg-bin/download_htext?htext=ko00001&format=htext&filedir=' -O ko00001.keg
+
+kegfile="ko00001.keg"
+
+while read -r prefix content
+do
+    case "$prefix" in A) col1="$content";; \
+                      B) col2="$content" ;; \
+                      C) col3="$content";; \
+                      D) echo -e "$col1\t$col2\t$col3\t$content";;
+    esac 
+done < <(sed '/^[#!+]/d;s/<[^>]*>//g;s/^./& /' < "$kegfile") > KO_Orthology_ko00001.txt
+
+
+### get KEGG_translation table
+
+
+KEGG_table = read_tsv("KO_Orthology_ko00001.txt", # see prep
+                col_names = c("Cat1","Cat2","Cat3","KEGG")) %>%
+  separate(., KEGG, into = c("KEGG_ID", "KEGG_desc"), sep = "  ") %>%
+  separate(., Cat1, into = c("Cat1_ID", "Cat1_Name"), 
+           sep = "\\s", extra = "merge") %>%
+  separate(., Cat2, into = c("Cat2_ID", "Cat2_Name"), 
+           sep = "\\s", extra = "merge") %>%
+  separate(., Cat3, into = c("Cat3_ID", "Cat3_Name"), 
+           sep = "\\s", extra = "merge")
+
+### read KEGGs table from DRAM output (annotations.tsv) ### 
+KEGG_samples = read_tsv("all_kegg.trm.tsv", 
+                        col_names = c("ORF_ID","SampleID","KEGG_ID", "KEGG_desc")) %>%
+  mutate(ORF_ID = gsub("^F[1-4,6,8][A-D]_", "", ORF_ID))
+
+SAMPLE = read_tsv("SAMPLE_genecalls_vs_F1A_combined.TPM.tsv",
+               col_names = c("ORF_ID", "TPM"), skip = 1) %>%
+  mutate(SampleID = "SAMPLE",
+         ORF_ID = gsub(" # .*", "", .$ORF_ID))
+
+all_samples = rbind(F1A, F1B, F1C, F1D,
+                    F2A, F2B, F2C, F2D,
+                    F3A, F3B, F3C, F3D,
+                    F4A, F4B, F4C, F4D,
+                    F6A, F6B, F6C, F6D,
+                    F8A, F8B, F8C, F8D) %>%
+  filter(TPM > 0)
+
+KEGGs = KEGG_samples %>%
+  left_join(all_samples, by = c("SampleID", "ORF_ID")) %>%
+  left_join(KEGG_table, by = "KEGG_ID")
