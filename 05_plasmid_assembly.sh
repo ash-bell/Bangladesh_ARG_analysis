daef9250075779e648f3667f920feb93c7a3d113
Ashley <44369148+ash-bell@users.noreply.github.com>
2022-07-19 13:43:47 +0100

    Update 05_plasmid_assembly.sh

--- 05_plasmid_assembly.sh
+++ 05_plasmid_assembly.sh
@@ -2,17 +2,17 @@
 metaplasmidspades.py \
 --only-assembler \
 --phred-offset 33 \
--s $PATH/ashley/10218_SAMPLE/merged.fq.gz \
---12 $PATH/ashley/10218_SAMPLE/qtrimmed.fq.gz 
--o $PATH/ashley/10218_SAMPLE/metaPlasmidSPAdes_output \
+-s $WKDIR/ashley/10218_SAMPLE/merged.fq.gz \
+--12 $WKDIR/ashley/10218_SAMPLE/qtrimmed.fq.gz 
+-o $WKDIR/ashley/10218_SAMPLE/metaPlasmidSPAdes_output \
 -m 1000 \
 -t 8
 
-#metaplasmidspades.py --continue -o $PATH/ashley/10218_SAMPLE/metaPlasmidSPAdes_output
+#metaplasmidspades.py --continue -o $WKDIR/ashley/10218_SAMPLE/metaPlasmidSPAdes_output
 
 ### detect plasmids from assembly using PlasClass ###
 python classify_fasta.py \
--f $PATH/ashley/plasmids/SAMPLE_plasmids_scaffolds.fasta \
+-f $WKDIR/ashley/plasmids/SAMPLE_plasmids_scaffolds.fasta \
 -o SAMPLE_plasmids.list \ # keep any contig with score > 0.5
 -p 16
 
@@ -30,7 +30,7 @@ checkv end_to_end \
 SAMPLE_VS2/vs2-pass1/final-viral-combined.fa \
 SAMPLE_VS2/checkv \
 -t 16 \
--d $PATH/ashley/tools/checkv-db-v1.0
+-d $WKDIR/ashley/tools/checkv-db-v1.0
 
 # remove any contig in "keep 1" / "keep 2" or "manual check" catagory. 
 # See here https://www.protocols.io/view/viral-sequence-identification-sop-with-virsorter2-5qpvoyqebg4o/v3?step=4
@@ -51,7 +51,7 @@ rgi main \
 ### get identity of genecalls on plasmids
 diamond blastx \
 --query interesting_plasmid.fnn \
---db $PATH/dbs/nr/nr.2021-07-09.diamond.2.0.11.dmnd \
+--db $WKDIR/dbs/nr/nr.2021-07-09.diamond.2.0.11.dmnd \
 --out dmd_plasmid.out \
 --outfmt 6 \
 --threads 16
